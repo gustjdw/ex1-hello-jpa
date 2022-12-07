0fa76a925247b9aa22ca1c675390f7418494e791
gustjdw <johs710@gachon.ac.kr>
2022-12-07 15:44:13 +0900

    영속성 관리-내부 동작 방식

--- src/main/java/jpabasic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/jpabasic/ex1hellojpa/hellojpa/JpaMain.java
@@ -17,31 +17,55 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member member = new Member();
 
-            // 등록
-//            member.setId(2L);
-//            member.setName("HelloB");
-//            em.persist(member);
+/*            // 비영속 상태
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
 
-            // 수정
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
+            // 이 시점에 영속 상태
+            System.out.println("=== Before persist ===");
+            em.persist(member);
+            System.out.println("=== After persist ===");
 
-            // 전체 조회
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
+            Member findMember = em.find(Member.class, 101L);  // 얘는 1차 캐시에서 조회했기 때문에 select 쿼리 안 나감
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            Member findMember2 = em.find(Member.class, 100L);  // 얘는 DB에서 조회했기 때문에 select 쿼리 나감
+            System.out.println("findMember2.getId() = " + findMember2.getId());
+            System.out.println("findMember2.getName() = " + findMember2.getName());
 
+            System.out.println("=== Before commit ===");
+            tx.commit();
+            System.out.println("=== After commit ===");*/
+
+/*            // select 쿼리가 1번만 나가야 함
+            Member findMember1 = em.find(Member.class, 101L);  // DB에서 조회, select 쿼리 나감
+            Member findMember2 = em.find(Member.class, 101L);  // 1차 캐시에서 조회, select 쿼리 안 나감*/
+
+/*            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("===========================================");
+
+            tx.commit();*/
+
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+            em.flush();
+            System.out.println("=============================================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
+            System.out.println("=== finally {em.close()}; ===");
             em.close();
         }
+        System.out.println("=== emf.close(); ===");
         emf.close();
     }
 }
