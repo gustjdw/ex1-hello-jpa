82ee1757f9f7a7a95c3df3e929227aeafe80d508
gustjdw <johs710@gachon.ac.kr>
2022-12-13 17:45:32 +0900

    임베디드 타입

--- src/main/java/jpabasic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/jpabasic/ex1hellojpa/hellojpa/JpaMain.java
@@ -8,49 +8,18 @@ import javax.persistence.Persistence;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
-
         tx.begin();
 
         try {
 
-//            Team team = new Team();
-//            team.setName("teamA");
-//            em.persist(team);
-//
-//            Member member1 = new Member();
-//            member1.setUsername("hello");
-//            member1.setTeam(team);
-//            em.persist(member1);
-//
-//            em.flush();
-//            em.clear();
-//
-//            Member m = em.find(Member.class, member1.getId());
-//            // team은 proxy로 나옴(LAZY일 때)
-//            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
-//
-//            System.out.println("=========================");
-//            m.getTeam().getName();  // team을 사용하는 시점에 초기화
-//            System.out.println("=========================");
+            Member member = new Member();
+            member.setUsername("hello");
+            member.setHomeAddress(new Address("city", "street", "10"));
+            member.setWorkPeriod(new Period());
 
-            Child child1 = new Child();
-            Child child2 = new Child();
-
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
-
-            em.persist(parent);
-
-            em.flush();
-            em.clear();
-
-            Parent findParent = em.find(Parent.class, parent.getId());
-            findParent.getChildList().remove(0);
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabasic/ex1hellojpa/hellojpa/Member.java
+++ src/main/java/jpabasic/ex1hellojpa/hellojpa/Member.java
@@ -1,9 +1,10 @@
 package jpabasic.ex1hellojpa.hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -12,25 +13,52 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @Embedded
+    private Period workPeriod;
+
+    @Embedded
+    private Address homeAddress;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
     public Team getTeam() {
         return team;
     }
+
     public void setTeam(Team team) {
         this.team = team;
     }
