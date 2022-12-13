a8b2133029350a0bb391210ce27665c773f6a6ba
gustjdw <johs710@gachon.ac.kr>
2022-12-13 16:17:45 +0900

    프록시와 연관관계 관리

--- src/main/java/jpabasic/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/jpabasic/ex1hellojpa/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,15 +17,40 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
+//            Team team = new Team();
+//            team.setName("teamA");
+//            em.persist(team);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("hello");
+//            member1.setTeam(team);
+//            em.persist(member1);
+//
+//            em.flush();
+//            em.clear();
+//
+//            Member m = em.find(Member.class, member1.getId());
+//            // team은 proxy로 나옴(LAZY일 때)
+//            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
+//
+//            System.out.println("=========================");
+//            m.getTeam().getName();  // team을 사용하는 시점에 초기화
+//            System.out.println("=========================");
 
-            em.persist(member);
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
 
             em.flush();
             em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();
         } catch (Exception e) {
